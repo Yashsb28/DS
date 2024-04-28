383eb269d255024a7b32d1e2085f860714384c54
Yash Bhoge <78728820+Yashsb28@users.noreply.github.com>
2024-04-29 01:31:46 +0530

    Delete square-20240428T195314Z-001 directory

--- square-20240428T195314Z-001/square/Serverintf.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-
-interface Serverintf extends Remote {
-
-    public boolean isPalindrome(String str) throws RemoteException;
-    public double square(double num) throws RemoteException;
-    public double squareRoot(double num) throws RemoteException;
-}
